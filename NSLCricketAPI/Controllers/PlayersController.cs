441a5668265b2b9538ca2bc53b8fef76bc2c4157
Dan Ilandarage <dhanuka.singhe@gmail.com>
2021-12-08 13:27:19 +1100

    Add project files.

---
+++ NSLCricketAPI/Controllers/PlayersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSLCricketAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlayersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Players()
+        {
+            var players = new string[] { "Sudheera_1", "Punyna", "Duviya", "Lalith" };
+            return Ok(players);
+        }
+
+        [HttpPost]
+        public IActionResult AddPlayer()
+        {
+            //var players = new string[] { "Sudheera", "Punyna", "Duviya" };
+            return BadRequest();
+        }
+    }
+}
